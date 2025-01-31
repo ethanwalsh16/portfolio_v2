f099426c80a0b6f7474f87a59a3761484800d834
ethanwalsh16 <ethancw16@gmail.com>
2025-01-31 13:09:33 -0500

    appearance fix

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -109,7 +109,7 @@ function App() {
 				
 				<p className="font-serif text-slate-900 ml-2">Range by David Epstein</p>
 
-					<p className="font-serif text-slate-900 ml-4 text-sm">The extended version of this book's title explains it well - 'why generalists triumph in a specialized world'</p>
+					<p className="font-serif text-slate-900 ml-4 text-sm">The extended version of this book's title explains it well - 'why generalists triumph in a specialized world'.</p>
 		</div>	
       </main>
       <div className="py-8"></div>
