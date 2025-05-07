01c7e990d479313eac0b56637433185823b9d7d1
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2025-05-06 20:07:03 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Experience = () => {
 	return (
 		<div className="bg-transparent pt-6">
 			<hr className="h-px bg-slate-200 border-0 md:w-[80%] w-full"></hr>
-			<h1 className="text-slate-800 md:text-sm text-xs md:py-4 py-2 pl-2 font-serif">Created by Ethan Walsh, 2024.</h1>
+			<h1 className="text-slate-800 md:text-sm text-xs md:py-4 py-2 pl-2 font-serif">Created by Ethan Walsh, 2025.</h1>
 		</div>
 	);
   };
