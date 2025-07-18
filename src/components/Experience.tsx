807f5f49e7826a27c627e6eeab1c9086e673a64d
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2025-07-17 22:24:49 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,8 +13,8 @@ const Experience: React.FC<ExperienceProps> = ({ title, company, oneliner, stack
 	return (
 		<div className="md:ml-4 ml-1">
 			<div className="col-span-1 py-3">
-				<p className="text-slate-900 pt-2 text-xl font-serif">{ title }</p>
-				<p className="text-slate-900 font-serif italic text-sm">{ company }</p>
+				<a href={link}><p className="text-slate-900 pt-2 text-xl font-serif">{ title }</p></a>
+				<a href={link}><p className="text-slate-900 font-serif italic text-sm">{ company }</p></a>
 			</div>
 			<div className="col-span-2 py-3 grid place-content-start space-y-4">
 				<p className="text-slate-900 text-sm font-serif">{ oneliner }</p>
