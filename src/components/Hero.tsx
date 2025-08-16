c23fa178c879db0fb0e2ffc15e8c57b153a1e6d2
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2025-08-16 11:09:28 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 					<p className="font-serif text-slate-900 pb-2"><i>Currently:</i></p>
 					<ul className="font-serif text-slate-900 text-md space-y-1">
 						<li>Studying software engineering at McMaster University</li>
-						<li>Working to grow the reach of McMaster's Artificial Intelligence community</li>
+						<li>Working at the Royal Bank of Canada on the real-time payments team</li>
 						<li>Making music with the engineering jazz band</li>
 						<li>Learning about digital ethics, how to get off your phone, and the return to physical media</li>
 					</ul>
