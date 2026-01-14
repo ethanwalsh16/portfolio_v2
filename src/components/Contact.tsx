dc2cc01f68f962c36fedc0b7a041813c9fe70849
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2026-01-13 20:44:52 -0500

    Update resume link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ const Contact: React.FC = () => {
 		<div className="space-x-4 md:ml-4 ml-1">
 			<a href="https://www.linkedin.com/in/ethanwalsh03" target="_blank" className="text-slate-900 font-serif hover:underline">LinkedIn</a>
 			<a href="https://github.com/ethanwalsh16" target="_blank" className="text-slate-900 font-serif hover:underline">GitHub</a>
-			<a href="https://drive.google.com/file/d/1W4WC92gjR8-rbE6IQmLj01gKPY-tPZ3p/view?usp=sharing" target="_blank" className="text-slate-900 font-serif hover:underline">Resume</a>
+			<a href="https://drive.google.com/file/d/1V3me6iGQKZoYJHQU57gLWrZ0nXNOzMw4/view?usp=sharing" target="_blank" className="text-slate-900 font-serif hover:underline">Resume</a>
 		</div>
 	);
 };
