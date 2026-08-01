61756895c21326b39797af1357d58e618c6b69ea
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2026-08-01 16:01:11 -0400

    Update hero for new content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import "../index.css"
 const Hero = () => {
 	return (
 		<div className="md:pb-16 pb-8 md:ml-4 ml-1">
-			<p className="font-serif text-slate-900 pb-2"><i>Current Snapshot:</i></p>
+			<p className="font-serif text-slate-900 pb-2"><i>Snapshot:</i></p>
 			<ul className="font-serif text-slate-900 text-md space-y-1">
-				<li>Building real-time payments infrastructure and microservices with Java, Spring Boot, and Camunda at the Royal Bank of Canada</li>
-				<li>Won the $20k technical distinction award and filed a provisional patent at RBC for a payment routing engine that optimizes the flow of international transactions</li>
-				<li>Studying software engineering at McMaster University</li>
-				<li>Creating toSCAN, an automated analyzer of privacy policies and TOS documents using natural language processing, spaCy, and Python</li>
+				<li>Working on the Cyber & Digital Engineering Team at Bruce Power, Canada's largest nuclear power facility</li>
+				<li>Previously at RBC & RBC Capital Markets, along with working as a team lead and Vice President for the McMaster AI Society</li>
+				<li>Won the $20k technical distinction award and filed a provisional patent at RBC for a payment routing engine that optimizes the flow of international wire transactions</li>
+				<li>Created pianAI, a computer vision tool to help learn the hand & wrist positioning for proper piano technique</li>
 				<li>Making music and performing with McMaster's engineering jazz band</li>
 			</ul>
 		</div>
