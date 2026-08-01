7e14715fbcb50673c27eecc370c472b5e0733014
Ethan Walsh <75648419+ethanwalsh16@users.noreply.github.com>
2026-08-01 16:01:44 -0400

    Remove Resume link from Contact component
    Removed the Resume link from the Contact component.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,6 @@ const Contact: React.FC = () => {
 		<div className="space-x-4 md:ml-4 ml-1">
 			<a href="https://www.linkedin.com/in/ethanwalsh03" target="_blank" className="text-slate-900 font-serif hover:underline">LinkedIn</a>
 			<a href="https://github.com/ethanwalsh16" target="_blank" className="text-slate-900 font-serif hover:underline">GitHub</a>
-			<a href="https://drive.google.com/file/d/1V3me6iGQKZoYJHQU57gLWrZ0nXNOzMw4/view?usp=sharing" target="_blank" className="text-slate-900 font-serif hover:underline">Resume</a>
 		</div>
 	);
 };
